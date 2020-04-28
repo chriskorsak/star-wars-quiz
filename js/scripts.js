ae9520cd8e5906feb3712368df131d10d37db730
chriskorsak <chris@chriskorsak.com>
2020-04-27 23:37:38 -0700

    add user interface logic for addition form

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,13 @@ var divide = function(number1, number2) {
 };
 
 // Interface logic
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#addOutput").text(result);
+  });
+});
